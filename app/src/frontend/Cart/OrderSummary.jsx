0d305670f445c7a91bcf23e754649a6863e05b25
AmIWhatIAm <96875017+AmIWhatIAm@users.noreply.github.com>
2024-09-06 02:05:43 +0800

    Fix bug

--- app/src/frontend/Cart/OrderSummary.jsx
+++ app/src/frontend/Cart/OrderSummary.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState } from "react";
 import PaymentMethodPage from "../pages/PaymentMethodPage";
 import { CartContext } from "./CartContext";
 import Modal from "../include/Modal/Modal";
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,15 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from "@react-oauth/google"
-import './index.css'
-
-import ProductPage from "./frontend/pages/ProductPage.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <GoogleOAuthProvider clientId='721278939294-754epiosjucfqahjktvlnt89f7j8o42b.apps.googleusercontent.com'>
   <React.StrictMode>
-    {/* <App /> */}
-    <ProductPage />
+    <App />
   </React.StrictMode>
   // </GoogleOAuthProvider>
 )
