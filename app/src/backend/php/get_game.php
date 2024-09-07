63913f6e178dcb1218b5e922ea82d2c86340887a
AmIWhatIAm <96875017+AmIWhatIAm@users.noreply.github.com>
2024-09-07 19:03:07 +0800

    switch branch je

--- app/src/backend/php/get_game.php
+++ app/src/backend/php/get_game.php
@@ -7,7 +7,7 @@ header('Access-Control-Allow-Headers: Content-Type');
 // Database connection parameters
 $servername = "localhost";
 $username = "root";
-$password = "teoH0628$$$$";
+$password = "";
 $dbname = "WAD_ASSIGNMENT";
 // Create a new connection
 $conn = new mysqli($servername, $username, $password, $dbname);
