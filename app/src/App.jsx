46a0bbb7e302e1f3e2d1ab6fd0ad31d47df9417f
DefinitelyNotTzeJing <144915477+DefinitelyNotTzeJing@users.noreply.github.com>
2024-09-07 21:50:04 +0800

    support page with email and others

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -9,6 +9,7 @@ import MainPage from "./frontend/pages/MainPage";
 import LoginPage from "./frontend/pages/LoginPage";
 import "./index.css";
 import { UserProvider } from "./frontend/pages/LoginContext";
+import Email from "./frontend/Email/email"; // for testing
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
                   <Route index element={<MainPage />} />
                   <Route path="/store" element={<StorePage />} />
                   <Route path="/store/:game_id" element={<ProductPage />} />
+                  <Route path="/support" element={<Email />} />
                   <Route path="/cart" element={<CartPage />} />
                   {/* for testing */}
                   {/* <Route path="/wishlist" element={<WishlistPage />} /> */}
