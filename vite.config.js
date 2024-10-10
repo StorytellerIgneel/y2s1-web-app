c75a49896a13c01ab97804081090e3eca7a68c5e
StorytellerIgneel <107390793+StorytellerIgneel@users.noreply.github.com>
2024-10-10 12:33:23 +0800

    chunk size

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/y2s1-web-app/',  // Set this to the repository name
+  chunkSizeWarningLimit: 1000, // or a size that fits your requirements
 })
